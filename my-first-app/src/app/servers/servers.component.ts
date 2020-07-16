e74d0690768933f29263231df08dd49271d2f233
karan1198 <kjoshi1110@gmail.com>
2020-07-16 21:20:37 +0530

    Passing and using data with Event Binding

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer =false;  
   serverCreationStatus ='no server is created!';
+  serverName = '';
 
   constructor() {
     setTimeout(() => {
@@ -21,5 +22,9 @@ export class ServersComponent implements OnInit {
   onCreateServer()
   {
     this.serverCreationStatus = 'Server was created!';
+  }
+
+  onUpdateServerName(event: Event){
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 }
