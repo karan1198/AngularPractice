108af1a38a2909d8552baf8838a053ca020bbff5
karan1198 <kjoshi1110@gmail.com>
2020-07-29 20:43:03 +0530

    11. Outputting a List of Recipes with ngFor

--- project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes =[];
+  recipes: Recipe[] =[
+    new Recipe('A Test Recipe', 'This is test', 'https://static.toiimg.com/thumb/msid-62757808,width-400,resizemode-4,imgsize-52519/62757808.jpg'),
+    new Recipe('A Test Recipe', 'This is test', 'https://static.toiimg.com/thumb/msid-62757808,width-400,resizemode-4,imgsize-52519/62757808.jpg') 
+  ];
 
   constructor() { }
 
